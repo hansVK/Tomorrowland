8e9599a772ebf778a7d9650cb4088df24df01a02
HansVK <hansak.vk@gmail.com>
2023-08-02 22:28:16 +0200

    1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "OnderliggendeBSysteem/OnderliggendeBSysteem.h"
+
+#include "MClassen/Gebruiker/Gebruiker.h"
+#include "MClassen/Park/Park.h"
+#include "MClassen/Tenant/Tenant.h"
+#include "MClassen/Aanvraag/Aanvraag.h"
+#include "MClassen/Recensie/Recensie.h"
+#include "MClassen/OccupyPark/OccupyPark.h"
+#include "MClassen/Abonnee/Abonnee.h"
+#include "MClassen/Datum/Datum.h"
+#include "vast.h"
+
+int main(){
+    OnderliggendeBSysteem os;
+}
